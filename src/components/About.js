45fab329a70e421b8a19b3e1b8e47ea5811099ce
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-04-16 22:08:00 +0300

    updated the navbar home and about js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I love building things with React and exploring the world of web development!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
+  
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,15 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>
+    <a href="#home">Home</a>
+    <div id="home">
+      <h1 style={{color: "firebrick"}}>
+        Your Name is a Web Developer from Your City
+      </h1>
+    </div>
+    <a href="#about">About</a>
+  </nav>;
 }
 
 export default NavBar;
